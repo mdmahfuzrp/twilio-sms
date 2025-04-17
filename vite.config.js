5b41d32204882c9bb50133291c385e16ef300d0e
mdmahfuzrp <mdmahfuzrp@gmail.com>
2025-04-18 01:56:22 +0600

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "tailwindcss"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/twilio-sms-web",
+  base: "/",
   server: {
     port: 3000,
   },
